df25641aeeb33d8646b4ed4e5ac6142e3831d26b
freya-scott <freya_scott@hotmail.co.uk>
2020-02-17 22:22:48 +0100

    part A completed(missing gnuplot load commands)

--- 1/es1module.f90
+++ 1/es1module.f90
@@ -13,22 +13,22 @@ contains
     implicit none
     integer, parameter :: size=5
     integer :: j
-    real, dimension(size) :: V, I, error
+    real(kind=rk), dimension(size) :: V, I, error
 
     open(unit=1, file='data.txt', action='read')
     do j=1,size
       read(1,*) V(j), I(j), error(j)
     end do
-    call formule(v,I,error)
+    call formule(V,I,error,size)
   end subroutine read_data
 
 
 
-  subroutine formule()
+  subroutine formule(V,I,error,size)
     implicit none
     real(kind=rk) :: s00,s10,s01,s20,s11,d
     integer :: size
-    real, dimension(size) :: V, I, error
+    real(kind=rk), dimension(size) :: V, I, error
 
     s00 = sum(1/error**2)
     s10 = sum(V/error**2)
@@ -37,17 +37,17 @@ contains
     s11 = sum(V*I/error**2)
     d = s00*s20 - s10**2
 
-    call stime(s00,s10,s01,s20,s11,d)
+    call stime(s00,s10,s01,s20,s11,d,I,V,size,error)
   end subroutine formule
 
 
 
-  subroutine stime(s00,s10,s01,s20,s11,d,I,V)
+  subroutine stime(s00,s10,s01,s20,s11,d,I,V,size,error)
     implicit none
     integer :: size
     real(kind=rk) :: s00,s10,s01,s20,s11,d
-    real(kind=rk) :: m,qsigma_m,sigma_q, cov
-    real(kind=rk), dimension(size) :: I,V
+    real(kind=rk) :: m,q,sigma_m,sigma_q, cov
+    real(kind=rk), dimension(size) :: I,V, error
 
     m = (s00*s11-s10*s01)/d
     q = (s20*s01-s10*s11)/d
@@ -55,19 +55,24 @@ contains
     sigma_q = sqrt(s20/d)
     cov = s10/d
 
-    call write_to_file(size,I,V,m,q)
+    call write_to_file(size,I,V,m,q,error)
   end subroutine stime
 
 
-  subroutine write_to_file()
+  subroutine write_to_file(size,I,V,m,q,error)
     implicit none
     integer :: size, j
-    real(kind=rk), dimension(size) :: I, V
+    real(kind=rk), dimension(size) :: I, V, I_stima, error
     real(kind=rk) :: m, q
 
     open(unit=2, file='m_q_stimati.txt', action='write')
     do j=1,size
-      write(2,*) I(j), m*V(j)+q
+      I_stima(j) = m*V(j)+q
+      write(2,*) I(j), I_stima(j), V(j), error(j), I(j)-I_stima(j)
     end do
+  end subroutine write_to_file
+
+!  first graph: gnuplot> plot 'm_q_stimati.txt' u 3:1:4 w err, '' u 3:2 w l
+!  second graph: gnuplot> plot 'm_q_stimati.txt' u 3:5:4 w err, 0 w l
 
 end module es1mod
